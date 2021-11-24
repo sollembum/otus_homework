6664d7ecfbc61f17bafc320e703a34e08a24ac9b
PogosianAK <PogosianAK@alfastrah.ru>
2021-11-24 23:35:47 +0300

    commit for first homework branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,11 @@ import service.QuizeService;
 
 public class Main {
     public static void main(String[] args) {
+
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("/spring-context.xml");
         QuizeService service = context.getBean(QuizeService.class);
         System.out.println(service.showQuestions());
+
     }
 }
