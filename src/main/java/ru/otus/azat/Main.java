49cf347ed61b06438068c22a4d2a8ffa26eb3f29
PogosianAK <PogosianAK@alfastrah.ru>
2021-12-29 01:22:42 +0300

    custom banner

--- src/main/java/ru/otus/azat/Main.java
+++ src/main/java/ru/otus/azat/Main.java
@@ -3,13 +3,10 @@ package ru.otus.azat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
 import ru.otus.azat.service.QuizeService;
 
-import javax.swing.*;
 
 @SpringBootApplication
 @PropertySource("classpath:application.properties")
