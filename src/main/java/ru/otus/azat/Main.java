4e8d29d8a25dce6586154cdefa8b344ef6b04016
PogosianAK <PogosianAK@alfastrah.ru>
2021-12-29 01:14:00 +0300

    thirdHomeWork dependency commit

--- src/main/java/ru/otus/azat/Main.java
+++ src/main/java/ru/otus/azat/Main.java
@@ -1,18 +1,23 @@
 package ru.otus.azat;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
 import ru.otus.azat.service.QuizeService;
 
-@ComponentScan
+import javax.swing.*;
+
+@SpringBootApplication
 @PropertySource("classpath:application.properties")
 public class Main {
     public static void main(String[] args) {
 
-        AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext(Main.class);
+        ApplicationContext context =
+                SpringApplication.run(Main.class, args);
         QuizeService service = context.getBean(QuizeService.class);
         service.startQuize();
     }
