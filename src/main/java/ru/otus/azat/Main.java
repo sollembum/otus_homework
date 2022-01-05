cd5ce545cd94dc9139891638765fb7e427bdd30f
PogosianAK <PogosianAK@alfastrah.ru>
2022-01-06 00:15:16 +0300

    файлы нужные для локализации

--- src/main/java/ru/otus/azat/Main.java
+++ src/main/java/ru/otus/azat/Main.java
@@ -3,13 +3,11 @@ package ru.otus.azat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.PropertySource;
 
 import ru.otus.azat.service.QuizeService;
 
 
 @SpringBootApplication
-@PropertySource("classpath:application.properties")
 public class Main {
     public static void main(String[] args) {
 
@@ -17,5 +15,6 @@ public class Main {
                 SpringApplication.run(Main.class, args);
         QuizeService service = context.getBean(QuizeService.class);
         service.startQuize();
+
     }
 }
