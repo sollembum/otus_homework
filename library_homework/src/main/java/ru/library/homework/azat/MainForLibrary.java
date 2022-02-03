6c8ff968311e4665a6d8a32e3b577738bd16f1cb
PogosianAK <PogosianAK@alfastrah.ru>
2022-02-03 14:39:55 +0300

    Deleted .idea from git

---
+++ library_homework/src/main/java/ru/library/homework/azat/MainForLibrary.java
@@ -0,0 +1,10 @@
+package ru.library.homework.azat;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainForLibrary {
+    public static void main(String[] args) {
+
+    }
+}
